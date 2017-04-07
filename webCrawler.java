bd1b955bb5534dd8509d54b97e356ccb8968fd45
Neville Lee <nevillealee@gmail.com>
2017-04-06 23:48:53 -0700

    Updated webCrawler.java to return 25 urls instead of 100. Crawler also uses FileWriter class to print urls to txt file named crawlResults which can be changed to any local .txt file

--- webCrawler.java
+++ webCrawler.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.net.URL;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class webCrawler{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		//Prompt user for URL
 		System.out.print("Enter a URL..");
 		String webAddy = new Scanner(System.in).next();
-		System.out.print(webAddy);
+	
 		//invoke crawler method with URL user entered as parameter
 		crawler(webAddy);
-	}	
+		
+			}	
+		
 		//crawler method
-		public static void crawler(String startUrl){
+		public static void crawler(String startUrl) throws IOException{
+		//create file writer to write urls's to crawlResults file
+		FileWriter writer = new FileWriter("C:\\Users\\Pavilion G7\\Desktop\\crawlResults.txt");
+		
 		// Make 2 ArrayLists to keep track of URL's to be added and Url's already traversed
 			ArrayList<String> pendingURL = new ArrayList<>();
 			ArrayList<String> finishedURL = new ArrayList<>();
@@ -21,29 +31,40 @@ public class webCrawler{
 			pendingURL.add(startUrl);
 		
 		//remove pending URL's and put them into urlString var as long as pending ArrayList isnt empty and finished ArrayList hasnt reached 100 urls
-		while(!pendingURL.isEmpty() && finishedURL.size() <= 100){
+		while(!pendingURL.isEmpty() && finishedURL.size() <= 25){
 			String urlString = pendingURL.remove(0);
 		// as long as url hasnt been checked already continue adding finished urls to finished ArrayList
 		if(!finishedURL.contains(urlString)){
 			finishedURL.add(urlString);
-			System.out.println("Crawled: " + urlString);
+			System.out.println("Craw " + urlString);
 		//calls subURLs method to take Strings (url from pending Array) and find sub urls within those pages and adds them to pending Array 
 		for(String s: getSubURLs(urlString)){
 			if(!finishedURL.contains(s))
 			pendingURL.add(s); 
 		    }	
 			}
+		// prints contents of finishedURL ArrayList to txt file by using FileWritter class	
+		for(String str: finishedURL) {
+				writer.write(str);
+					}
+				
 		  }
+		  // close file
+		  writer.close();
 		}
 		
 		//method to retrieve suburls to add to pendingArray
 		public static ArrayList<String> getSubURLs(String urlString) {
+			//creat new ArrayList to hold sub urls
 			ArrayList<String> list = new ArrayList<>();
 			
 			try {
+				//covert string url into java url object
 				URL url = new URL(urlString);
+				//scanner that reads url page
 				Scanner input = new Scanner(url.openStream());
 				int current = 0;
+				//loop will continue as lonng as their are more tokens to be read from current url
 				while(input.hasNext()) {
 					String line = input.nextLine();
 					current = line.indexOf("http:", current);
@@ -58,13 +79,14 @@ public class webCrawler{
 					}
 				}
 			}
+			// catches any exception getSubURLs method may throw i.e. IOException, ArrayIndexOutOfBoundsException..
 			catch (Exception ex) {
 				System.out.println("Error: " + ex.getMessage());
 				}
 				return list;
 		}
 			
-			
+	
 	
 }
 
